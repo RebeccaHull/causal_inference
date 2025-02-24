d7f1314afc8df46add87dfe88f4a56735b7631a8
Rebecca Hull <140359800+RebeccaHull@users.noreply.github.com>
2025-02-24 14:05:22 -0700

    more class on feb 24 2025

--- code/intro_to_pymc.py
+++ code/intro_to_pymc.py
@@ -2,3 +2,15 @@ import numpy as np
 import polars as pl
 import pymc as pm
 import arviz as az
+
+np.random.seed(42)
+
+# Set the parameter values.
+beta0 = 3
+beta1 = 7
+sigma = 3
+n = 100
+
+# Simulate Data
+x = np.random.uniform(0, 7, size = n)
+y = beta0 + beta1 * x + np.random.normal(0, sigma, size = n)
