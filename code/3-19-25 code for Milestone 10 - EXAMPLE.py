dca70a2cb12a27b7956126ca22ed9ccf72d8d47e
Rebecca Hull <140359800+RebeccaHull@users.noreply.github.com>
2025-03-19 13:50:57 -0600

    ta da

--- code/3-19-25 code for Milestone 10 - EXAMPLE.py
+++ code/3-19-25 code for Milestone 10 - EXAMPLE.py
@@ -1,2 +1,13 @@
 #3-19-25 code for Milestone 10 - EXAMPLE
 
+import arviz as az
+import numpy as np
+import pandas as pd
+import pymc as pm
+import seaborn sns
+
+#Outcomes
+
+def outcome(t, control_intercept, treat_intercept_delta, trend, Δ, group, treated):
+    return control_intercept + (treat_intercept_delta * group) + (trend * t) + (Δ * treated * group)
+
